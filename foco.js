0e8090a7e98492a43bd786bec212c5a42108d3c3
Alejandro Chacón <85378121+witchdrums@users.noreply.github.com>
2023-10-20 16:47:56 -0600

    fdsa

--- foco.js
+++ foco.js
@@ -9,4 +9,23 @@ function foco(){
     const anio = new Date().getFullYear();
     titulo.innerText = `Desarrollo de Sistemas Web ${anio}`;
     document.body.appendChild(titulo);
+
+    const img = document.createElement("img");
+    img.classList.add("foco");
+    img.setAttribute("src","foco-apagado.jpg");
+
+    img.addEventListener("click", () => {
+        if (img.src.indexOf("apagado") > 0){
+            img.setAttribute("src", "foco-prendido.jpg");
+            document.body.classList.remove("oscuridad");
+            document.body.classList.add("luz");
+
+        }else{
+            img.setAttribute("src", "foco-apagado.jpg");
+            document.body.classList.remove("luz");
+            document.body.classList.add("oscuridad");
+        }
+    });
+
+    document.body.appendChild(img);
 }
